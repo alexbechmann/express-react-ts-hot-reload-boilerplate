b2a021a6b99e1a573e150cce225dab8376ef2aba
Alex Bechmann <alex.bechmann@outlook.com>
2018-12-17 09:06:30 +0100

    updated deps

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -3,20 +3,16 @@ import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import App from './App';
 
-render(
-  <AppContainer>
-    <App />
-  </AppContainer>,
-  document.getElementById('root')
-);
+const renderApp = () =>
+  render(
+    <AppContainer>
+      <App />
+    </AppContainer>,
+    document.getElementById('root')
+  );
+
+renderApp();
 
 if (module.hot) {
-  module.hot.accept('./App', () => {
-    render(
-      <AppContainer>
-        <App />
-      </AppContainer>,
-      document.getElementById('root')
-    );
-  });
+  module.hot.accept('./App', renderApp);
 }
--- webpack.config.client.js
+++ webpack.config.client.js
@@ -11,6 +11,7 @@ module.exports = {
     "./src/client/index",
   ],
   target: "web",
+  mode: "development",
   module: {
     rules: [
       {
